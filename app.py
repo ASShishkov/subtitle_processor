e01c7e65ec9512ebd075f6d28e699d9b077f9f67
Aguha <asshishkov2012@gmail.com>
2025-05-30 12:13:44 +0400

    Первый коммит на ветке go_to_sqlite

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ class SubtitleFilterApp(QMainWindow):
         self.setMinimumSize(600, 400)
 
         # Устанавливаем начальный размер (меньше чем было)
-        self.resize(1250, 800)
+        self.resize(1250, 801)
 
         # Позиционируем окно в центре экрана
         self.center_window()
